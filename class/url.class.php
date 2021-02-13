3409196f3fb5f2661ba2708d25db3df323cd7580
fengyu <867548082@qq.com>
2021-02-13 13:16:33 +0800

    微信域名检测

---
+++ class/url.class.php
@@ -0,0 +1,78 @@
+<?php
+class Url {
+    public $conn;
+    
+    public function __construct() {
+        
+    }
+    
+    public function check_url($urls) {
+        if ($urls) {
+            $norm = [];
+            $wx_abn = [];
+            $dns_abn = [];
+            $url_arr = explode(',', $urls);
+            for ($i = 0; $i < count($url_arr); $i++) {
+                preg_match('/[a-zA-z]+:\/\/[^\s]*/', $url_arr[$i], $url);
+                if ($url[0]) {
+                    if ($this->http_code($url[0]) == 200) {
+                        echo get_headers('https://qq.com');
+                        exit();
+                        $headers = get_headers('http://mp.weixinbridge.com/mp/wapredirect?url='.$url[0]);
+                        $Location = $headers[6];
+                        if (preg_match('/Location: http/', $Location) == 1) {
+                            if ($Location == 'Location: '.$url[0]) {
+                                $norm[] = $url[0];
+                            } else {
+                                $wx_abn[] = $url[0];
+                            }
+                        } else {
+                            return array(
+                                'code' => 202,
+                                'msg' => '查询接口错误，请及时处理'
+                            );
+                            exit();
+                        }
+                    } else {
+                        $dns_abn[] = $url[0];
+                    }
+                } else {
+                    return array(
+                        'code' => 201,
+                        'msg' => 'url参数错误，请传入带https或http的链接，多个链接用英文逗号分隔'
+                    );
+                    exit();
+                }
+            }
+            $res = array(
+                'code' => 200,
+                'data' => array(
+                    'norm' => $norm,
+                    'wx_abn' => $wx_abn,
+                    'dns_abn' => $dns_abn
+                ),
+                'msg' => '检测成功'
+            );
+        } else {
+            $res = array(
+                'code' => 201,
+                'msg' => '缺少url参数'
+            );
+        }
+        return $res;
+    }
+    
+    private function http_code($url){
+        $ch = curl_init();
+        $timeout = 3;
+        curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_exec($ch);
+        return $httpcode = curl_getinfo($ch,CURLINFO_HTTP_CODE);
+        curl_close($ch);
+    }
+}
+?>
---
+++ url.php
@@ -0,0 +1,29 @@
+<?php
+header('Content-type:application/json');
+require_once('./class/url.class.php');
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    require('/Applications/phpstudy/WWW/weixin/curl.php');
+} else {
+    require_once('/www/wwwroot/'.$_SERVER['SERVER_NAME'].'/curl.php');
+}
+
+$urls = $_GET['url'];
+$server = $_GET['server'];
+
+$url = new Url();
+$res = $url->check_url($urls);
+if ($server) {
+    $server = substr($server, -1) == '/' ? $server : $server.'/';
+    if ($res['code'] == 200) {
+        $wx_abn = $res['data']['wx_abn'];
+        if (count($wx_abn) > 0) {
+            $wx_abn = str_replace(array('http://', 'https://'), '', implode('、', $res['data']['wx_abn']));
+            get_url($server.'微信域名检测'.'/'.$wx_abn.'已被微信屏蔽，请及时处理');
+        }
+    } elseif ($res['code'] == 202) {
+        get_url($server.'微信域名检测'.'/'.$res['msg']);
+    }
+}
+
+echo json_encode($res);
+?>
